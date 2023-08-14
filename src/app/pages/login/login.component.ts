6c07498b93499dd2568ea3c0ab36fd906679b9c4
maher <maher.karzoun@gmail.com>
2023-08-14 15:23:28 +0200

    style login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ValidatorEmail, ValidatorPassword } from 'src/app/utils/regex';
-import {delay, debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -10,31 +9,23 @@ import {delay, debounceTime} from 'rxjs/operators';
 })
 export class LoginComponent {
 
-  public login: FormGroup;
-  public showError ?: boolean;
+  public loginForm: FormGroup;
 
   constructor(formBuilder: FormBuilder) {
-    this.login = formBuilder.group({
-      username: ['', [Validators.required,ValidatorEmail]],
-      password: ['', [Validators.required,ValidatorPassword]]
+    this.loginForm = formBuilder.group({
+      username: ['', [Validators.required, ValidatorEmail]],
+      password: ['', [Validators.required, ValidatorPassword]]
     })
   }
 
-
-  ngOnInit(): void {
-    this.login.valueChanges.pipe(
-      delay(500),
-      debounceTime(1000)
-    ).subscribe(()=> this.showError = !this.login.valid  )
-  }
-  
-  reset(){
-    this.login.reset({})
+  reset() {
+    this.loginForm.reset({})
   }
 
-  submit(){
-    if(!this.login.valid){
+  login() {
+    if (this.loginForm.valid) {
 
     }
+
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     spacing: {
-      '1': '8px',
-      '2': '12px',
-      '3': '16px',
-      '4': '24px',
-      '5': '32px',
-      '6': '48px',
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
     },
     colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      "t-color": "#e0e1e7",
+      form: {
+        origin: "#272953",
+        dark: "#16113a",
+        lighe: "#04cbf8",
+        header:"#0fd354"
+      },
+      dashboard: {
+        dark: "#5F4842",
+        lighe: "#EDBFC6",
+      },
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
     },
     extend: {
       spacing: {
-        '128': '32rem',
-        '144': '36rem',
+        128: "32rem",
+        144: "36rem",
       },
       borderRadius: {
-        '4xl': '2rem',
-      }
-    }
+        "4xl": "2rem",
+      },
+    },
   },
   plugins: [],
-}
-
+};
